9cc2cb82b1c535f0e63b8ae6ba8e72ceebb45526
Matthias <vidalmat06@gmail.com>
2020-12-15 15:43:11 +0100

    modif

--- users.php
+++ users.php
@@ -4,7 +4,6 @@ $user1 = ["pseudo" => "Jean", "password" => "azert"];
 $user2 = ["pseudo" => "Tom", "password" => "azerty"];
 $user3 = ["pseudo" => "Jade", "password" => "ytreza"];
 
-
 $users = [$user1, $user2, $user3];
 
 // var_dump($users);
