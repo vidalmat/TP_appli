17b0f40db1e55753fe90ac0a39b1990119c8acd6
Matthias <vidalmat06@gmail.com>
2020-12-15 15:41:08 +0100

    commit

--- users.php
+++ users.php
@@ -4,6 +4,7 @@ $user1 = ["pseudo" => "Jean", "password" => "azert"];
 $user2 = ["pseudo" => "Tom", "password" => "azerty"];
 $user3 = ["pseudo" => "Jade", "password" => "ytreza"];
 
+
 $users = [$user1, $user2, $user3];
 
 // var_dump($users);
