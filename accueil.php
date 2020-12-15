32d1c58b051b98eeef67dc4cc2667397a8e66331
Matthias <vidalmat06@gmail.com>
2020-12-15 19:00:05 +0100

    modif

--- accueil.php
+++ accueil.php
@@ -5,34 +5,46 @@ require_once("index.php");
 
 ?>
 
+<div class="conteneur_connexion">
 
+    <div class="creation_compte">
         
 
-<h1>Vous n'avez pas de compte?</h1>
+        <h1 class="parconnexion">Vous n'avez pas de compte?</h1>
 
 
-<form action="index.php?route=insertuser" method="POST">
+        <form action="index.php?route=insertuser" method="POST">
 
-    <input type="text" placeholder="Pseudo" name="pseudo" required><br>
+            <input type="text" placeholder="Pseudo" name="pseudo" required><br>
+                        
+            <input type="password" placeholder="Mot de passe" name="password" required ><br>
+
+            <input type="password" placeholder="Confirmez le mot de passe" name="password2" required ><br>
+            
+            <input type="submit" id='submit' value='Inscription' >
+
+        </form>
+
+    </div>
+
+    <span class="span2"></span>
+
+    <div class="connexion">
+
+
+        <form action="index.php?route=connectuser" method="POST">
+            
+                <h2>Se connecter</h2>
+            
+                <input type="text" placeholder="Pseudo" name="pseudo" required>
                 
-    <input type="password" placeholder="Mot de passe" name="password" required ><br>
+                <input type="password" placeholder="Mot de passe" name="password" required >
+                
+                <input type="submit" id='submit' value='Envoyer'>
+            
+        </form>
 
-    <input type="password" placeholder="Confirmez le mot de passe" name="password2" required ><br>
-    
-    <input type="submit" id='submit' value='Inscription' >
+    </div>
 
-</form>
-
-
-<form action="index.php?route=connectuser" method="POST">
-    
-        <h2>Se connecter</h2>
-    
-        <input type="text" placeholder="Pseudo" name="pseudo" required>
-        
-        <input type="password" placeholder="Mot de passe" name="password" required >
-        
-        <input type="submit" id='submit' value='Envoyer' >
-    
-</form>
+</div>
 
